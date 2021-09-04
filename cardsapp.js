b47842b72f4b803fd2267029b4b486c167fa5d43
mcodemax <mwalterjohnson7@gmail.com>
2021-09-03 20:47:07 -0700

    drawn card shows on DOM

--- cardsapp.js
+++ cardsapp.js
@@ -1,6 +1,12 @@
 const baseUrl = `http://deckofcardsapi.com/api/`;
+const $btn = $('button');
+const $cardDisplay = $('#card-display');
+let deck;
+let cardsDrawn = 0;
+let currentCard;
 
-$.getJSON(`${baseUrl}deck/new/draw/?count=1`)
+function test(){
+    $.getJSON(`${baseUrl}deck/new/draw/?count=1`)
     .then(data => {
         console.log(data.cards)
 
@@ -11,22 +17,93 @@ $.getJSON(`${baseUrl}deck/new/draw/?count=1`)
 
 
 
-//2
-let deck;
-let card1;
-$.getJSON(`${baseUrl}deck/new/draw/?count=1`)
-    .then(data => {
-        console.log(`p2----------`);
-        
-        deck = data.deck_id;
-        card1 = data.cards[0]
-        console.log({deck, card1});
-        return $.getJSON(`${baseUrl}deck/${deck}/draw/?count=1`);
-    })
-    .then(data => {
-        console.log({data})
-        console.log(`${card1.value} of ${card1.suit}`)
-        console.log(`${data.cards[0].value} of ${data.cards[0].suit}`)
-    });
+    //2
+    let deck;
+    let card1;
+    $.getJSON(`${baseUrl}deck/new/draw/?count=1`)
+        .then(data => {
+            console.log(`p2----------`);
+            
+            deck = data.deck_id;
+            card1 = data.cards[0]
+            console.log({deck, card1});
+            return $.getJSON(`${baseUrl}deck/${deck}/draw/?count=1`);
+        })
+        .then(data => {
+            console.log({data})
+            console.log(`${card1.value} of ${card1.suit}`)
+            console.log(`${data.cards[0].value} of ${data.cards[0].suit}`)
+        });
 
-////
+};
+
+/**Build an HTML page that lets you draw cards from a deck. When the page loads, go
+ *  to the Deck of Cards API to create a new deck, and show a button on the page that
+ *  will let you draw a card. Every time
+ *  you click the button, display a new card, until there are no cards left
+ *  in the deck. */
+
+//make jquery event listener on html element btn that draws a card
+//when card is drawn, add it to "card-display"
+
+//https://api.jquery.com/ready/ 
+//jquery func waits for DOM to load below
+$(function() {
+
+
+    $btn.click(function(){ //handles drawing new card
+
+        //if deck undef or outta cards
+        if(cardsDrawn >= 52){//52 cards in deck
+            return alert(`The Deck is Empty!`)
+        }
+
+        if(!deck){//for this assignment keep drawing cards till dekc runs out
+            $.getJSON(`${baseUrl}deck/new/draw/?count=1`)
+            .then(data => {
+                console.log(`drawing first card from new gen deck...`);
+                
+                deck = data.deck_id;
+                cardsDrawn = 1;//1 card drawn when drawing from new deck
+                currentCard = data.cards[0]
+                console.log({currentCard})
+
+                showCard(currentCard);
+
+                return $.getJSON(`${baseUrl}deck/${deck}/draw/?count=1`);
+            })
+            
+        }else{
+                
+            $.getJSON(`${baseUrl}deck/${deck}/draw/?count=1`)
+            .then(data => {
+                
+                console.log(`drawing from deck currently in play...`);
+                $cardDisplay.empty()
+                cardsDrawn++;
+
+                currentCard = data.cards[0]
+                console.log({currentCard});                
+
+                showCard(currentCard);
+            });
+            // .then(data => {
+            //     console.log({data})
+            //     console.log(`${card1.value} of ${card1.suit}`)
+            //     console.log(`${data.cards[0].value} of ${data.cards[0].suit}`)
+            // });
+        }
+
+        //append currentCard.image to the div id="card-display" element
+
+    });
+    
+  });
+
+  function showCard({image}){//needs input of card
+    $cardDisplay.append(`<div id="current-card">
+        <img src="${image}" alt="">
+        </div>`);
+  }
+
+  
